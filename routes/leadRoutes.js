1d599c1d7834e2bb25db4d2213190f4e9db0bd1e
SAPPA SAI <sappasai.msccs@gmail.com>
2025-08-08 18:13:14 +0530

    updated the route

--- routes/leadRoutes.js
+++ routes/leadRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createLead, getLeads, updateLeadStatus, updateLead, deleteLead } = require("../controllers/leadController");
+const { createLead, getLeads, updateLeadStatus, updateLead, deleteLead, } = require("../controllers/leadController");
 const authenticateToken = require("../middleware/authMiddleware");
 
+const { getAnalytics } = require("../controllers/analyticsController");
+
+router.get("/analytics", authenticateToken, getAnalytics);
 router.post("/", authenticateToken, createLead);
 router.get("/", authenticateToken, getLeads);
 // router.post("/assign", authenticateToken, assignLead);
@@ -12,4 +15,3 @@ router.delete("/:id", authenticateToken, deleteLead);
 // router.get("/assigned", authenticateToken, getAssignedLeads); // New route
 
 module.exports = router;
-
