24a86ff01af394381e57ef2fe09008737659365d
B.K.S.S.S MALLIK <sssmallik143@gmail.com>
2025-08-22 17:32:47 +0530

    port updated

--- src/server.js
+++ src/server.js
@@ -45,5 +45,5 @@ app.use('/', (req, res) => {
   res.send('LMT Backend is running');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
